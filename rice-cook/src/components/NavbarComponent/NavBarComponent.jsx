1ecebab636ddeaccf9575cb79c4099e1fab75acb
MinhAi1311 <minhkid1410@gmail.com>
2024-01-02 23:30:41 +0700

    Up
    none

---
+++ rice-cook/src/components/NavbarComponent/NavBarComponent.jsx
@@ -0,0 +1,76 @@
+import { Checkbox, Rate } from "antd";
+import React from "react";
+import {
+  WrapperContent,
+  WrapperLableText,
+  WrapperTextPrice,
+  WrapperTextValue,
+} from "./style";
+
+const NavBarComponent = () => {
+  const onChange = () => {};
+  const renderContent = (type, options) => {
+    switch (type) {
+      case "text":
+        return options.map((option) => {
+          return <WrapperTextValue>{option}</WrapperTextValue>;
+        });
+      case "checkbox":
+        return (
+          <Checkbox.Group
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "12px",
+            }}
+            onChange={onChange}
+          >
+            {options.map((option) => {
+              return (
+                <Checkbox style={{ marginLeft: 0 }} value={option.value}>
+                  {option.label}
+                </Checkbox>
+              );
+            })}
+          </Checkbox.Group>
+        );
+      case "star":
+        return options.map((option) => {
+          return (
+            <div style={{ dispaly: "flex" }}>
+              <Rate
+                style={{ fontSize: "12px" }}
+                disabled
+                defaultValue={option}
+              />
+              <span> {`tu ${option}  sao`}</span>
+            </div>
+          );
+        });
+      case "price":
+        return options.map((option) => {
+          return <WrapperTextPrice>{option}</WrapperTextPrice>;
+        });
+      default:
+        return {};
+    }
+  };
+
+  return (
+    <div>
+      <WrapperLableText>Hãng Sản Phẩm</WrapperLableText>
+      <WrapperContent>
+        {renderContent("text", [
+          "Toshiba",
+          "Philips",
+          "Sharp",
+          "Kangaroo",
+          "Sunhouse",
+        ])}
+      </WrapperContent>
+    </div>
+  );
+};
+
+export default NavBarComponent;
